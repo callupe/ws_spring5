bdb4b42ba887840b51b90ee77bb135d4ac093195
callupe <callupe@gmail.com>
2018-12-18 06:12:14 -0500

    Primera  carga

---
+++ spring/spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into clientes (nombre, apellido, email, create_at) values('Rafael','Callupe','cc@mail.com','2018-01-01');
+insert into clientes (nombre, apellido, email, create_at) values('Juan','Calle','cc1@mail.com','2018-01-01');
+insert into clientes (nombre, apellido, email, create_at) values('Carlos','Blass','cc2@mail.com','2018-01-01');
+insert into clientes (nombre, apellido, email, create_at) values('Marita','Romero','cc4@mail.com','2018-01-01');
